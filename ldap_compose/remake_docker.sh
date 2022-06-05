d1c2bc87b31f1760d6f9722b989719673255f91e
xingzhe <54618163+2892211452@users.noreply.github.com>
2022-06-06 01:14:19 +0800

    update

--- ldap_compose/remake_docker.sh
+++ ldap_compose/remake_docker.sh
@@ -1,19 +1,19 @@
 rm -rf ./data/ldapdb/*
 rm -rf ./data/LdapConfig/*
-#
-#docker_name="openldap"
-#docker image prune -f
-#docker stop ${docker_name}
-#docker rm ${docker_name}
-#
-#docker_name="phpldapadmin"
-#docker image prune -f
-#docker stop ${docker_name}
-#docker rm ${docker_name}
-#
-#echo -e "\033[5;36mOrz 旧容器(镜像)已清理\033[0m"
-#
-#
-#docker-compose -f docker-compose.yml up -d
-#sleep 1
-#docker ps -a
+
+docker_name="openldap"
+docker image prune -f
+docker stop ${docker_name}
+docker rm ${docker_name}
+
+docker_name="phpldapadmin"
+docker image prune -f
+docker stop ${docker_name}
+docker rm ${docker_name}
+
+echo -e "\033[5;36mOrz 旧容器(镜像)已清理\033[0m"
+
+
+docker-compose -f docker-compose.yml up -d
+sleep 1
+docker ps -a
