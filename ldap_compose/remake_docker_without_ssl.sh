7da6d79e1ca3200cbab845b255e0d2879b090566
“kengerlwl” <kengerlwl@qq.com>
2023-05-27 15:02:02 +0800

    update

--- ldap_compose/remake_docker_without_ssl.sh
+++ ldap_compose/remake_docker_without_ssl.sh
@@ -14,6 +14,6 @@ docker rm ${docker_name}
 echo -e "\033[5;36mOrz 旧容器(镜像)已清理\033[0m"
 
 
-docker-compose -f docker-compose.yml up -d
+docker-compose -f docker-compose_without_ssl.yml up -d
 sleep 1
 docker ps -a
